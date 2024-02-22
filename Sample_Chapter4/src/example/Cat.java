ffea9068ea1f0252ab1757dea89b295c31b4248f
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-22 09:42:30 +0900

    Lesson27まで学習完了

--- Sample_Chapter4/src/example/Cat.java
+++ Sample_Chapter4/src/example/Cat.java
@@ -1,15 +1,27 @@
 package example;
 
 public class Cat {
+    String name;
+    boolean hungry;
+
     void eat() {
-        System.out.println("ご飯を食べるよ！おいしいにゃ～");
+        System.out.println(name + "> ご飯を食べるよ！おいしいにゃ～");
+        System.out.println(name + "> お腹が一杯になったにゃ～");
+        hungry = false;
     }
 
     boolean isHungry() {
-        return true;
+        return hungry;
     }
 
     void playToy(String toy) {
-        System.out.println(toy + "で遊ぶよ。楽しいにゃ～");
+        System.out.println(name + "> " + toy + "で遊ぶよ。楽しいにゃ～");
+        System.out.println(name + "> 遊んでお腹が減ったにゃ～");
+        hungry = true;
+    }
+
+    void someMethod() {
+        name = "タマ";
+        System.out.println(name);
     }
 }
--- Sample_Chapter4/src/example/UseCatSample.java
+++ Sample_Chapter4/src/example/UseCatSample.java
@@ -3,6 +3,7 @@ package example;
 public class UseCatSample {
     public static void main(String[] args) {
         Cat tama = new Cat();
+        tama.name = "タマ";
         tama.eat();
         tama.playToy("ひも");
         boolean h = tama.isHungry();
